fb24f7a90cf97486fb34c53950b28745a1c074b5
riyandrmd <ryanda.rama10@gmail.com>
2024-03-06 22:21:07 +0700

    create controller

--- main.go
+++ main.go
@@ -1,34 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"golang-todos/controller"
 	"golang-todos/database"
-	"net/http"
 
 	"github.com/labstack/echo"
 )
-
-type CreateRequest struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-}
-
-type UpdateRequest struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-}
-
-type CheckRequest struct {
-	Done bool `json:"done"`
-}
-
-type TodoResponse struct {
-	Id          int    `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Done        bool   `json:"done"`
-}
 
 func main() {
 	db := database.InitDb()
@@ -41,112 +18,11 @@ func main() {
 
 	e := echo.New()
 
-	e.GET("/todos", func(ctx echo.Context) error {
-		rows, err := db.Query("SELECT * FROM todos")
-		if err != nil {
-			return ctx.String(http.StatusInternalServerError, err.Error())
-		}
-
-		var res []TodoResponse
-		for rows.Next() {
-			var id int
-			var title string
-			var description string
-			var done int
-
-			err = rows.Scan(&id, &title, &description, &done)
-			if err != nil {
-				return ctx.String(http.StatusInternalServerError, err.Error())
-			}
-
-			var todo TodoResponse
-			todo.Id = id
-			todo.Title = title
-			todo.Description = description
-			if done == 1 {
-				todo.Done = true
-			}
-			res = append(res, todo)
-		}
-
-		return ctx.JSON(http.StatusOK, res)
-	})
-
-	e.PATCH("/todos/:id/check", func(ctx echo.Context) error {
-		id := ctx.Param("id")
-
-		var request CheckRequest
-		json.NewDecoder(ctx.Request().Body).Decode(&request)
-
-		var doneInt int
-		if request.Done {
-			doneInt = 1
-		}
-
-		_, err := db.Exec(
-			"UPDATE todos SET done = ? WHERE id = ?",
-			doneInt,
-			id,
-		)
-
-		if err != nil {
-			return ctx.String(http.StatusInternalServerError, err.Error())
-		}
-
-		return ctx.String(http.StatusOK, "OK")
-	})
-
-	e.PATCH("/todos/:id", func(ctx echo.Context) error {
-		id := ctx.Param("id")
-
-		var request UpdateRequest
-		json.NewDecoder(ctx.Request().Body).Decode(&request)
-
-		_, err := db.Exec(
-			"UPDATE todos SET title = ?, description = ? WHERE id = ?",
-			request.Title,
-			request.Description,
-			id,
-		)
-
-		if err != nil {
-			return ctx.String(http.StatusInternalServerError, err.Error())
-		}
-
-		return ctx.String(http.StatusOK, "OK")
-	})
-
-	e.POST("/todos", func(ctx echo.Context) error {
-
-		var request CreateRequest
-		json.NewDecoder(ctx.Request().Body).Decode(&request)
-
-		_, err := db.Exec(
-			"INSERT INTO todos (title, description, done) VALUES (?, ?, 0)",
-			request.Title,
-			request.Description,
-		)
-
-		if err != nil {
-			return ctx.String(http.StatusInternalServerError, err.Error())
-		}
-
-		fmt.Println(request)
-		return ctx.String(http.StatusOK, "OK")
-	})
-
-	e.DELETE("/todos/:id", func(ctx echo.Context) error {
-		id := ctx.Param("id")
-
-		_, err := db.Exec(
-			"DELETE FROM todos WHERE id = ?",
-			id,
-		)
-		if err != nil {
-			return ctx.String(http.StatusInternalServerError, err.Error())
-		}
-		return ctx.String(http.StatusOK, "OK")
-	})
+	controller.NewGetALLTodosController(e, db)
+	controller.NewCreateTodoContoller(e, db)
+	controller.NewUpdateTodoController(e, db)
+	controller.NewDeleteTodoController(e, db)
+	controller.NewCheckTodoController(e, db)
 
 	e.Start(":8080")
 }
